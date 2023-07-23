4e0856895141ccae4fdb1273ca042c32f864a2ee
Roland Hilliges <scott.lang@posteo.net>
2023-07-23 13:42:24 -0600

    configured harpoon

---
+++ after/plugin/harpoon.lua
@@ -0,0 +1,51 @@
+local wk = require 'which-key'
+
+local h = require("harpoon")
+local hm = require 'harpoon.mark'
+local hu = require 'harpoon.ui'
+h.setup({
+    tabline = true,
+})
+
+wk.register({
+    ['<leader>'] = {
+        p = {
+            name = '[p]in',
+            f = { function()
+                hm.toggle_file()
+            end,
+                'toggle [f]ile'
+            },
+        },
+        ['1'] = {
+            function()
+                hu.nav_file(1)
+            end,
+            'first buffer'
+        },
+        ['2'] = {
+            function()
+                hu.nav_file(2)
+            end,
+            'second buffer'
+        },
+        ['3'] = {
+            function()
+                hu.nav_file(3)
+            end,
+            'third buffer'
+        },
+        ['4'] = {
+            function()
+                hu.nav_file(4)
+            end,
+            'fourth buffer'
+        },
+    }
+})
+
+vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
+vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
+vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
