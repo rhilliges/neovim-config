1df84aa33605aecbc28b6ac1d70a0a63d69a6461
brolyn <scott.lang@posteo.net>
2024-04-28 20:48:24 -0600

    added themes

--- lua/brolyn/plugins/theme.lua
+++ lua/brolyn/plugins/theme.lua
@@ -1,12 +1,41 @@
 return {
-    "catppuccin/nvim", name = "catppuccin", priority = 1000,
-    config = function ()
-        require"catppuccin".setup({
-            transparent_background = true,
-            integrations = {
-                fidget = true
-            }
-        })
-        vim.cmd.colorscheme "catppuccin"
-    end
+    {
+         "rose-pine/neovim",
+         name = "rose-pine",
+        priority = 1000,
+        config = function ()
+            require"rose-pine".setup({
+                -- variant = "dawn"
+                -- transparent_background = false,
+                -- integrations = {
+                --     fidget = true
+                -- }
+            })
+            vim.cmd.colorscheme "rose-pine"
+        end
+    }, {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function ()
+            require"catppuccin".setup({
+                transparent_background = false,
+                integrations = {
+                    fidget = true
+                }
+            })
+            -- vim.cmd.colorscheme "catppuccin-latte"
+        end
+    },
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require"tokyonight".setup {
+              style = "day"
+        }
+        -- vim.cmd.colorscheme "tokyonight"
+      end
+    }
 }
