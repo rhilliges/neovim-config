a48e9e40b8bcfce542007f9c746751660ddf3cba
brolyn <32675635+rhilliges@users.noreply.github.com>
2024-07-29 08:06:24 -0600

    @git; close dap ui when opening git state

--- lua/brolyn/plugins/git.lua
+++ lua/brolyn/plugins/git.lua
@@ -2,11 +2,23 @@ return {
     {
         "tpope/vim-fugitive",
         config = function()
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+            vim.keymap.set("n", "<leader>gs", function ()
+                local ok, mod = pcall(require, "dapui")
+                if ok then
+                    mod.close()
+
+                end
+
+                vim.cmd.Git()
+            end)
             local builtin = require 'telescope.builtin'
             vim.keymap.set("n", "<leader>sb", builtin.git_branches)
             vim.keymap.set("n", "<leader>sc", builtin.git_commits)
         end
     },
-    'f-person/git-blame.nvim'
+    'f-person/git-blame.nvim',
+    {
+        'lewis6991/gitsigns.nvim',
+        opts = {}
+    }
 }
