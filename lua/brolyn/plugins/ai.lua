b9f4b72d3263d450c1e4f594047e1ed15d3fe0cf
brolyn <32675635+rhilliges@users.noreply.github.com>
2025-08-01 14:06:56 -0600

    update ai config

--- lua/brolyn/plugins/ai.lua
+++ lua/brolyn/plugins/ai.lua
@@ -1,49 +1,60 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  opts = {
-    -- add any opts here
-    -- for example
-    provider = "bedrock",
-  },
-  init = function()
-  end,
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	opts = {
+		-- add any opts here
+		-- for example
+		provider = "bedrock",
+		providers = {
+			bedrock = {
+				disable_tools = true, -- disable tools!
+				model = "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
+				aws_profile = "bedrock",
+				aws_region = "us-east-1",
+				token_counting = false,
+				cache = {
+					enabled = false,
+				},
+			},
+		},
+	},
+	init = function() end,
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		{
+			-- support for image pasting
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				-- recommended settings
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+					-- required for Windows users
+					use_absolute_path = true,
+				},
+			},
+		},
+		-- {
+		-- 	-- Make sure to set this up properly if you have lazy=true
+		-- 	"MeanderingProgrammer/render-markdown.nvim",
+		-- 	opts = {
+		-- 		file_types = { "markdown", "Avante" },
+		-- 	},
+		-- 	ft = { "markdown", "Avante" },
+		-- },
+	},
 }
